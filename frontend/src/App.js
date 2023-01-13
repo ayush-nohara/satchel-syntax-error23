1918839fc2bdab4d6ac18c36bd8fc78a7c4737db
Aman Lachhiramka <amanlachhiramka09@gmail.com>
2023-01-14 00:32:35 +0530

    Navbar modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./containers/footer/Footer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="gradient_bg">
         <Navbar />
       </div>
+      <Footer />
     </div>
   );
 }
--- frontend/src/containers/footer/Footer.js
+++ frontend/src/containers/footer/Footer.js
@@ -1,11 +1,48 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
+import NavbarLogo from "./assets/satchelLogo.jpeg";
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={styles.footer}>
+      <div className={styles.footerHeading}>
+        <h1>Footer</h1>
+      </div>
+      <div className={styles.footerBtn}>
+        <p>Hello from Satchel</p>
+      </div>
 
-export default Footer
+      <div className={styles.footerLinks}>
+        <div className={styles.footerLinksLogo}>
+          <img src={NavbarLogo} alt="Satchel" />
+          <p>Hello from Satchel</p>
+        </div>
+        <div className={styles.footerLinksDiv}>
+          <h4>Links</h4>
+          <p>Facebook</p>
+          <p>Facebook</p>
+          <p>Facebook</p>
+          <p>Facebook</p>
+        </div>
+        <div className={styles.footerLinksDiv}>
+          <h4>About us</h4>
+          <p>Facebook</p>
+          <p>Facebook</p>
+          <p>Facebook</p>
+          <p>Facebook</p>
+        </div>
+        <div className={styles.footerLinksDiv}>
+          <h4>Get in Touch</h4>
+          <p>Facebook</p>
+          <p>Facebook</p>
+          <p>Facebook</p>
+          <p>Facebook</p>
+        </div>
+      </div>
+      <div className={styles.footerCopyright}>
+        <p>copyright 2023. All rights reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
