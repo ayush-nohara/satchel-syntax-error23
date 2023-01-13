e3d7d2df0741b199981124a8e72e781b98a9b0f1
Samarendra Gouda <samarendragouda@gmail.com>
2023-01-14 01:18:16 +0530

    dashboard

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Card from '../Card/Card';
 import eth from './assets/ethereum.svg';
+import polygon from './assets/polygon.svg';
+import avax from './assets/avax.svg';
 import styles from './Dashboard.module.css';
 
 const Dashboard = (props) => {
 
     return (
         <div className={styles.container}>
-            <Card image={eth}/>
-            <Card image={eth}/>
-            <Card image={eth}/>
+            <Card image={eth} name={"GoerliETH"} balance={"5.00 ETH"}/>
+            <Card image={polygon} name={"Mumbai"} balance={"10.00 MATIC"}/>
+            <Card image={avax} name={"Fuji"} balance={"1.00 AVAX"}/>
         </div>
     );
 };
