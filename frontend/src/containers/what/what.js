58248d4444ae60679a35d6fc145b78bbff0d0a1e
Prabhat1308 <94048855+Prabhat1308@users.noreply.github.com>
2023-01-15 03:38:04 +0530

    Update what.js

--- frontend/src/containers/what/what.js
+++ frontend/src/containers/what/what.js
@@ -8,11 +8,11 @@ const what = () => {
       <hr className={styles.line}/>
       <p className={styles.heading}>
         Wanna do a crypto transaction? Satchel got you covered! It allows you to
-        send your crypto holdings to another metamask wallet, just in a flash
-        click!.<br></br> Satchel has done a cakewalk for you to know your metamask
-        balance on Three different testnets simultaneously, cool isn’t it! <br></br> Wait,
+        send your crypto holdings to another metamask wallet even on different chains, just in a flash
+        click!.<br></br> Satchel has made it a cakewalk for you to know your metamask
+        balance on three different main-nets simultaneously, cool isn’t it! <br></br> Wait,
         you thought we can’t do more than this, right? Satchel also saves your
-        time if you want to swap your tokens between two metamask wallets.
+        time if you want to swap your tokens between two metamask wallets even cross-chain.
       </p>
     </div>
   );
